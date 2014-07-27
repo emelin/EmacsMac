1c6f3b76ecb45a130776db4042efaa6b2e333ee1
Yukang <moorekang@gmail.com>
2014-07-28 01:20:17 +0800

    backup

--- init.el
+++ init.el
@@ -9,8 +9,6 @@
   (package-initialize)
   (add-to-list 'package-archives
                '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (add-to-list 'package-archives
-	       '("marmalade" . "http://marmalade-repo.org/packages/") t)
   )
 
 (require 'package)
